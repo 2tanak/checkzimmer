1404842dba2f19a3688751598c07aede2757dc56
Max <max@sharlaev.com>
2021-02-12 12:42:15 +0300

    Подготовка формы к отправке

--- resources/views/single/inquiry-modal.blade.php
+++ resources/views/single/inquiry-modal.blade.php
@@ -6,40 +6,42 @@
             </svg>
         </div>
         <div class="title">{{ __('Send request') }}</div>
-        <form class="inquiry-form">
-            <input type="hidden" name="grecaptcha" value="">
+        <form>
             <div class="input-block-item">
-                <input id="company" name="company" type="text">
+                <input id="company" type="text">
                 <label for="company">{{ __('Company name') }}</label>
             </div>
             <div class="input-block-item">
-                <input id="name" name="name" type="text">
+                <input id="name" type="text">
                 <label for="name">{{ __('Name and Surname') }}*</label>
+                <div class="validation-block">{{ __('Please fill in this field') }}</div>
             </div>
             <div class="input-block-item">
-                <input id="email" name="email" type="email">
+                <input id="email" type="email">
                 <label for="email">{{ __('Email') }}*</label>
+                <div class="validation-block">{{ __('Please fill in this field') }}</div>
             </div>
             <div class="input-block-item">
-                <input id="telephone" name="phone" type="tel">
+                <input id="telephone" type="tel">
                 <label for="telephone">{{ __('Phone number') }}*</label>
+                <div class="validation-block">{{ __('Please fill in this field') }}</div>
             </div>
             <div class="data-block">
-                <input id="arrival-date" name="date-arrival" data-provide="datepicker" readonly>
+                <input id="arrival-date" data-provide="datepicker" readonly>
                 <label for="arrival-date">{{ __('Arrival date') }}</label>
                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M13 0H14V2H16C16.5523 2 17 2.44772 17 3V6V7V17C17 17.5523 16.5523 18 16 18H2C1.44772 18 1 17.5523 1 17V7V6V3C1 2.44772 1.44772 2 2 2H4V0H5V2H13V0ZM5 3H4H2V6H16V3H14H13H5ZM16 17H2V7H16V17ZM10 12H14V13H10V12ZM14 10H10V11H14V10ZM4 12H8V13H4V12ZM8 10H4V11H8V10Z" fill="#7A8793"/>
                 </svg>
             </div>
             <div class="data-block">
-                <input id="date-departure" name="date-departure" data-provide="datepicker" readonly>
+                <input id="date-departure" data-provide="datepicker" readonly>
                 <label for="date-departure">{{ __('Date departure') }}</label>
                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M13 0H14V2H16C16.5523 2 17 2.44772 17 3V6V7V17C17 17.5523 16.5523 18 16 18H2C1.44772 18 1 17.5523 1 17V7V6V3C1 2.44772 1.44772 2 2 2H4V0H5V2H13V0ZM5 3H4H2V6H16V3H14H13H5ZM16 17H2V7H16V17ZM10 12H14V13H10V12ZM14 10H10V11H14V10ZM4 12H8V13H4V12ZM8 10H4V11H8V10Z" fill="#7A8793"/>
                 </svg>
             </div>
             <div class="select-block">
-                <select id="number-persons" name="number-persons">
+                <select id="number-persons">
                     <option>1 {{ __('Person') }}</option>
                     <option>2 {{ __('Persons') }}</option>
                     <option>3 {{ __('Persons') }}</option>
@@ -50,31 +52,26 @@
                 <label for="number-persons">{{ __('Number persons') }}</label>
             </div>
             <div class="select-block">
-                <select id="type" name="type">
+                <select id="type">
                     <option>{{ __('Whole apartment') }}</option>
                     <option>{{ __('House') }}</option>
                     <option>{{ __('Room') }}</option>
                     <option>{{ __('Garage') }}</option>
                 </select>
-                <label for="number-persons" >{{ __('Housing type') }}</label>
+                <label for="number-persons">{{ __('Housing type') }}</label>
             </div>
             <div class="text-area-block">
                 <textarea placeholder="{{ __('Message to owner') }}"></textarea>
             </div>
             <div class="checkbox-block checkbox-middle">
-                <input id="email-checkbox" class="checkbox" type="checkbox" name="email-copy">
+                <input id="email-checkbox" class="checkbox" type="checkbox">
                 <label for="email-checkbox">{{ __('Send me a copy by email') }}</label>
             </div>
             <div class="checkbox-block checkbox-top">
-                <input id="consent-checkbox" class="checkbox" type="checkbox" name="consent">
+                <input id="consent-checkbox" class="checkbox" type="checkbox">
                 <label for="consent-checkbox">{{ __('I consent to the processing of my data as described in') }} <a href="#">{{ __('statement of consent') }}</a> {{ __('from') }} Checkzimmer.</label>
             </div>
             <a class="send-request" href="#">{{ __('Send request') }}</a>
         </form>
     </div>
 </div>
-<script>
-    jQuery('.inquiry-form').submit({
-
-    });
-</script>
