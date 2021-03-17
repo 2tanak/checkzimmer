e557e9ece2e5936e9a6fb662dfe1a1ca60c6818b
Andrey Gorshkov <megabgg@yandex.ru>
2021-03-17 13:52:01 +0300

    Исправление CheckLocale.php. Убрал лишний код после return в методе handle.

--- app/Http/Middleware/CheckLocale.php
+++ app/Http/Middleware/CheckLocale.php
@@ -14,11 +14,5 @@ class CheckLocale
         $locale = $request->segment(1) ?? $defaultLocale;
         App::setLocale($locale);
         return $next($request);
-        dd($request);
-        if (Auth::user()->role === 2) {
-            return $next($request);
-        } else {
-            return response()->json(['error' => 'Unauthorized'], 403);
-        }
     }
 }
