680818b73e1a7eb54a7dbad51f41f31f98494009
Max Sharlaev <max@sharlaev.com>
2021-04-09 12:33:24 +0300

    Подсказка для поиска

--- resources/js/components/Public/Home.vue
+++ resources/js/components/Public/Home.vue
@@ -37,8 +37,41 @@ export default {
                 axios.post('/search/tooltip', { input: e.target.value})
                 .then( (resp) => {
                     that.searchRes = resp.data;
+                    if (that.searchRes.length === 0) {
+                        jQuery('.result-search ul').html('<li>' + that.$t('No results found') + '</li>');
+                        jQuery('.result-search').addClass('active');
+                        return;
+                    }
+                    let str = [];
+                    for (let i in that.searchRes) {
+                        let line = `<li type="${that.searchRes[i]['type']}" domain="${that.searchRes[i]['domain']}">${that.searchRes[i]['text']}</li>`;
+                        str.push(line)
+                    }
+                    jQuery('.result-search ul').html(str);
+                    jQuery('.result-search').addClass('active');
                 })
             }, 1000)
+        })
+        jQuery('.result-search').on('click', 'ul li', function(e) {
+            let type = jQuery(this).attr('type');
+            if (!type) {
+                jQuery('.result-search').removeClass('active');
+                return;
+            }
+            let domain = jQuery(this).attr('domain');
+            let subdomain = domain.split('//')[1].split('.')[0];
+            let current = document.location.hostname.split('.')[0];
+            if (current !== subdomain) {
+                console.log(domain + '?' + jQuery('form.find-subdomain-redirect').serialize());
+                //document.location = domain + '?' + jQuery('form.find-subdomain-redirect').serialize();
+            }
+
+        })
+        jQuery('body').on('click', function(e) {
+            if (jQuery(this).closest('.result-search').length === 0) {
+                jQuery('.result-search').removeClass('active');
+            }
+
         })
 
     },
