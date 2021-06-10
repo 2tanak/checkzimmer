adc8aa889d24924e85b0c5968369a585789e8b16
Max Sharlaev <max@sharlaev.com>
2021-06-10 12:52:03 +0300

    Правка блока количества человек на главной странице

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,27 +61,10 @@
                             <label class="desctop-label" for="number-personse">{{ __('Num people') }}</label>
                             <div class="select-container">
                                 <select name="person" id="number-personse" class="number-personse">
-                                    <option value="1" selected>1 {{ __('person') }}</option>
-                                    <option value="2">2 {{ __('persons') }}</option>
-                                    <option value="3">3 {{ __('persons') }}</option>
-                                    <option value="4">4 {{ __('persons') }}</option>
-                                    <option value="5">5 {{ __('persons') }}</option>
-                                    <option value="6">6 {{ __('persons') }}</option>
-                                    <option value="7">7 {{ __('persons') }}</option>
-                                    <option value="8">8 {{ __('persons') }}</option>
-                                    <option value="9">9 {{ __('persons') }}</option>
-                                    <option value="10">10 {{ __('persons') }}</option>
-                                    <option value="11">11 {{ __('persons') }}</option>
-                                    <option value="12">12 {{ __('persons') }}</option>
-                                    <option value="13">13 {{ __('persons') }}</option>
-                                    <option value="14">14 {{ __('persons') }}</option>
-                                    <option value="15">15 {{ __('persons') }}</option>
-                                    <option value="16">16 {{ __('persons') }}</option>
-                                    <option value="17">17 {{ __('persons') }}</option>
-                                    <option value="18">18 {{ __('persons') }}</option>
-                                    <option value="19">19 {{ __('persons') }}</option>
-                                    <option value="20">20 {{ __('persons') }}</option>
-                                    <option value="21">20+ {{ __('persons') }}</option>
+                                    <option value="1" {{ 1 == ($getData['person'] ?? 1) ? 'selected' : '' }}>1 {{ __('person') }}</option>
+                                    @foreach(range(2, 21, 1) as $person)
+                                        <option value="{{ $person }}" {{ $person == ($getData['person'] ?? 1) ? 'selected' : '' }}>{{ $person <= 20 ? $person : '20+' }} {{ __('persons') }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
