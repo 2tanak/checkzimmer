3b9134e1d11054cf5c64b52b42f407c012acc464
Andrey Gorshkov <megabgg@yandex.ru>
2021-07-08 02:51:51 +0300

    Вывод в шаблон письма информации о удобствах

--- resources/views/email/registration.blade.php
+++ resources/views/email/registration.blade.php
@@ -3,6 +3,7 @@
             <div class="main-registration-content">
                 @include('email.registration.account', ['data' => $data])
                 @include('email.registration.contact', ['data' => $data])
+                @include('email.registration.facilities', ['data' => $data])
                 @include('email.registration.property-data', ['data' => $data])
                 @include('email.registration.photos', ['data' => $data])
 
--- resources/views/email/registration/facilities.blade.php
+++ resources/views/email/registration/facilities.blade.php
@@ -1,1 +1,12 @@
-<?php
+<h2>Facilities information</h2>
+
+<table>
+    <tr>
+        <td>Name</td>
+    </tr>
+    @foreach($data['property']['facilities'] as $facility_id)
+        <tr>
+            <td>{{ \App\Feature::find($facility_id)->first()->name }}</td>
+        </tr>
+    @endforeach
+</table>
