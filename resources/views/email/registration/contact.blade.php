853265ece9b4f2541a899c27a9b3b3f08c27c06a
Олег <oleg@orange-pie.ru>
2021-07-07 19:48:29 +0300

    Стилизация письма, которое получает клиент после регистрации

--- resources/views/email/registration/contact.blade.php
+++ resources/views/email/registration/contact.blade.php
@@ -1,52 +1,67 @@
-<h2>Contact information</h2>
+<div class="title-block" style="margin-bottom:20px;">
+    <div style="width:3px;height:20px;background: linear-gradient(232.88deg, #0F7FEB -6.69%, #00CEFF 114.04%);margin-right:10px;display:inline-block;vertical-align:middle;margin-left:5px;"></div>
+    <h2 style="margin-top:0;margin-bottom:0;vertical-align:middle;display:inline-block;">Contact information</h2>
+</div>
 
-<table>
+<table style="width:100%;margin-bottom:40px;">
     <tr>
-        <td>Contact person:</td>
-        <td>{{ $data['contact']['person']['name'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Contact person:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['person']['name'] }}</td>
     </tr>
     <tr>
-        <td>Email:</td>
-        <td>{{ $data['contact']['email'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Email:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['email'] }}</td>
     </tr>
     <tr>
-        <td>Display email:</td>
-        <td>{{ $data['contact']['email_display'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Display email:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['email_display'] }}</td>
     </tr>
     <tr>
-        <td>Phone number:</td>
-        <td>{{ $data['contact']['phone'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Phone number:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['phone'] }}</td>
     </tr>
     <tr>
-        <td>Show phone number:</td>
-        <td>{{ $data['contact']['phone_display'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Show phone number:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['phone_display'] }}</td>
     </tr>
     <tr>
-        <td>Enable whatsapp:</td>
-        <td>{{ $data['contact']['phone_whatsapp'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Enable whatsapp:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['phone_whatsapp'] }}</td>
     </tr>
     <tr>
-        <td>Additional phone:</td>
-        <td>{{ $data['contact']['phoneAdditional'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Additional phone:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['phoneAdditional'] }}</td>
     </tr>
     <tr>
-        <td>Show additional phone:</td>
-        <td>{{ $data['contact']['phoneAdditional_display'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Show additional phone:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;;">{{ $data['contact']['phoneAdditional_display'] }}</td>
     </tr>
     <tr>
-        <td>Use whatsapp for additional phone:</td>
-        <td>{{ $data['contact']['phoneAdditional_whatsapp'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Use whatsapp for additional phone:</td>
+        <td style="width:10%;"></td>
+        <td style=width:80%;">{{ $data['contact']['phoneAdditional_whatsapp'] }}</td>
     </tr>
     <tr>
-        <td>Landline phone:</td>
-        <td>{{ $data['contact']['phoneStat'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Landline phone:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['phoneStat'] }}</td>
     </tr>
     <tr>
-        <td>Show landine phone:</td>
-        <td>{{ $data['contact']['phoneStat_display'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">Show landine phone:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['phoneStat_display'] }}</td>
     </tr>
     <tr>
-        <td>FAX:</td>
-        <td>{{ $data['contact']['phone_fax'] }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">FAX:</td>
+        <td style="width:10%;"></td>
+        <td style="width:80%;">{{ $data['contact']['phone_fax'] }}</td>
     </tr>
 </table>
--- resources/views/email/registration/property-data.blade.php
+++ resources/views/email/registration/property-data.blade.php
@@ -1,25 +1,28 @@
-<h2>Peoperty information</h2>
+<div class="title-block" style="margin-bottom:20px;">
+    <div style="width:3px;height:20px;background: linear-gradient(232.88deg, #0F7FEB -6.69%, #00CEFF 114.04%);margin-right:10px;display:inline-block;vertical-align:middle;margin-left:5px;"></div>
+    <h2 style="margin-top:0;margin-bottom:0;vertical-align:middle;display:inline-block;">Peoperty information</h2>
+</div>
 
-<table>
+<table style="width:100%;margin-bottom:40px;">
     <tr>
-        <td>Peoperty</td>
-        <td>Number</td>
-        <td>Persons</td>
-        <td>Price</td>
+        <td style="width: 25%;background-color:#f3f3f3;">Peoperty</td>
+        <td style="text-align:center;width: 25%;background-color:#f3f3f3;">Number</td>
+        <td style="text-align:center;width: 25%;background-color:#f3f3f3;">Persons</td>
+        <td style="text-align:center;width: 25%;background-color:#f3f3f3;">Price</td>
     </tr>
     @foreach($data['property']['propertyTypes'] as $type)
         <tr>
-            <td>{{ $type['name'] }}</td>
-            <td>{{ $type['num'] }}</td>
-            <td>{{ $type['persons'] }}</td>
-            <td>{{ $type['price'] }}</td>
+            <td style="width: 25%;">{{ $type['name'] }}</td>
+            <td style="text-align:center;width: 25%;">{{ $type['num'] }}</td>
+            <td style="text-align:center;width: 25%;">{{ $type['persons'] }}</td>
+            <td style="text-align:center;width: 25%;">{{ $type['price'] }}</td>
         </tr>
         @foreach ($type['rooms'] as $room)
             <tr>
-                <td>{{ $room['name'] }}</td>
-                <td>{{ $room['num'] }}</td>
-                <td>{{ $room['persons'] }}</td>
-                <td>{{ $room['price'] }}</td>
+                <td style="width: 25%;">{{ $room['name'] }}</td>
+                <td style="text-align:center;width: 25%;">{{ $room['num'] }}</td>
+                <td style="text-align:center;width: 25%;">{{ $room['persons'] }}</td>
+                <td style="text-align:center;width: 25%;">{{ $room['price'] }}</td>
             </tr>
         @endforeach
     @endforeach
