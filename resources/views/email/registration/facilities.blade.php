d7ada3dec7c452627ab6e24407a4ea27271c62b0
Max Sharlaev <max@sharlaev.com>
2021-07-08 14:57:48 +0300

    Стилизация блока удобств в письме-уведомлении

--- resources/views/email/registration/facilities.blade.php
+++ resources/views/email/registration/facilities.blade.php
@@ -1,13 +1,16 @@
-<h2>Facilities information</h2>
+<div class="title-block" style="margin-bottom:20px;">
+    <div style="width:3px;height:20px;background: linear-gradient(232.88deg, #0F7FEB -6.69%, #00CEFF 114.04%);margin-right:10px;display:inline-block;vertical-align:middle;margin-left:5px;"></div>
+    <h2 style="margin-top:0;margin-bottom:0;vertical-align:middle;display:inline-block;">Facilities information</h2>
+</div>
 
 <table>
     <tr>
-        <td>{{ __('Category') }}</td>
+        <td style="width:20%;background-color:#f3f3f3;">{{ __('Category') }}</td>
         <td>{{ __('Name') }}</td>
     </tr>
     @foreach($data['property']['features'] as $feature)
         <tr>
-            <td>{{ $feature->feature_category->name }}</td>
+            <td style="width:20%;background-color:#f3f3f3;">{{ $feature->feature_category->name }}</td>
             <td><img src="{{ $data['domain'].$feature->picture }}" /> {{ $feature->name }}</td>
         </tr>
     @endforeach
