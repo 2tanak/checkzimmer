56129a28307d9067fdf5c92845094b835cfd035c
Олег <oleg@orange-pie.ru>
2021-08-17 09:25:29 +0300

    Правки картинок на странице ловушки. Разбиение на отдельные компоненты blade на странице ловушки

---
+++ resources/views/inquiry/advertising.blade.php
@@ -0,0 +1,46 @@
+<div class="advertising">
+
+    <div class="advertising-item">
+        <div class="img-container">
+            <img src="/img/rectangle139.png" alt="">
+        </div>
+        <div>
+            <div class="advertising-title">
+                {{ __('Who we are and why you can rely on us') }}
+            </div>
+            <div class="advertising-text">
+                <p>{{ __('Since 2015 we have been your loyal, reliable and competent companion looking for one perfect fitters room. We have 5678 accommodations across Germany') }}.</p>
+                <p>{{ __('Wir sind ein service-orientiertes Unternehmen, bei dem Kundenzufriedenheit oberste Priorität hat') }}.</p>
+                <p style="font-weight:600">{{ __('Whatever we do, we do it for you and with all of our hearts') }}.</p>
+                <p>{{ __('The awareness of our agency is growing steadily, so that we already have an extensive database of landlords, partners and regular customers') }}.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="advertising-item">
+        <div style="text-align:center;max-width:580px;margin:auto;">
+            <div class="advertising-title">
+                {{ __('Our team') }}
+            </div>
+            <div class="advertising-text">
+                <p style="font-weight:600">{{ __('We pull together') }}.</p>
+                <p>{{ __('It is our goal to meet your wishes and needs quickly, efficiently and to your complete satisfaction. We will answer your questions quickly and guarantee an offer tailored to you') }}.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="advertising-item">
+        <div class="img-container">
+            <img src="/img/rectangle140.png" alt="">
+        </div>
+        <div>
+            <div class="advertising-title">
+                {{ __('We have made your comfort our main task') }}
+            </div>
+            <div class="advertising-text">
+                <p>{{ __('All rooms have a TV and WiFi and usually have separate beds. All apartments also have fully equipped kitchens (fridge, oven, stove, cutlery, plates, kettle, etc.). Each employee has a single bed, their own closet and enough space to relax in our accommodation') }}.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
